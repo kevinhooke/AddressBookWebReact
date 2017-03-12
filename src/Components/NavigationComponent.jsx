65b66465b9bc0f07c9bd654dfb52f4e6b79523b9
Kevin Hooke <kevin.hooke@gmail.com>
2017-03-12 12:23:53 -0700

    Updated react-router and added Dockerfiles

--- src/Components/NavigationComponent.jsx
+++ src/Components/NavigationComponent.jsx
@@ -1,25 +1,31 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router';
+import React, {Component} from 'react';
+import {Link} from 'react-router';
 
-class NavigationComponent extends Component{
+class NavMenu extends Component {
 
-    render(){
-        return(
-            <nav className="navbar navbar-default" role="navigation">
-                <div className="navbar-header">
-                    <Link className="navbar-brand" to="/">AddressBook</Link>
+    render() {
+        return (
+            <div>
+                <div>
+                    <nav className="navbar navbar-default" role="navigation">
+                        <div className="navbar-header">
+                            <Link className="navbar-brand" to="/">AddressBook</Link>
+                        </div>
+
+                        <div className="collapse navbar-collapse navbar-ex1-collapse">
+                            <ul className="nav navbar-nav">
+                                <li className="active"><Link to="search">Search</Link></li>
+                                <li><Link to="new">New</Link></li>
+                            </ul>
+                        </div>
+                    </nav>
                 </div>
 
-                <div className="collapse navbar-collapse navbar-ex1-collapse">
-                    <ul className="nav navbar-nav">
-                        <li className="active"><Link to="/search">Search</Link></li>
-                        <li><Link to="/new">New</Link></li>
-                    </ul>
+                <div>
+                    {this.props.children}
                 </div>
-            </nav>
-
-
+            </div>
         )
     }
 }
-export default NavigationComponent;
+export default NavMenu;
--- src/Containers/App.jsx
+++ src/Containers/App.jsx
@@ -3,21 +3,19 @@ import { Router, Route, IndexRoute, Link, hashHistory } from 'react-router';
 import Home from '../Components/HomeComponent';
 import AddNew from '../Components/AddNewComponent';
 import Search from '../Components/SearchComponent';
-import NavigationComponent from '../Components/NavigationComponent';
+import NavMenu from '../Components/NavigationComponent';
 
 class App extends Component {
 
     render() {
         return (
-            <div className="App">
                 <Router history={hashHistory}>
-                    <Route path="/" component={NavigationComponent}>
-                    <IndexRoute component={Home} />
-                    <Route path="new" component={AddNew} />
-                    <Route path="search" component={Search} />
+                    <Route path="/" component={NavMenu}>
+                        <IndexRoute component={Home} />
+                        <Route path="new" component={AddNew} />
+                        <Route path="search" component={Search} />
                     </Route>
                 </Router>
-            </div>
         );
     }
 }
